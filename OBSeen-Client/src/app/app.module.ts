245a62e9f1a35dbbda6268b31aa97900d4a17be5
Aaron Ford <c3030408@uon.edu.au>
2017-11-25 20:45:42 +1100

    Created Basic File structure and added some started components/services/models
    Signed-off-by: Aaron Ford <c3030408@uon.edu.au>

--- OBSeen-Client/src/app/app.module.ts
+++ OBSeen-Client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AgendaListComponent } from './components/agenda-list/agenda-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgendaListComponent
   ],
   imports: [
     BrowserModule
